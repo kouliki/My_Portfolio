7c6b930dda2c7bdb3497d1b5e57f1b3180a71769
Kouliki Mahato <95127992+kouliki@users.noreply.github.com>
2025-01-26 15:48:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Flutter Developer", "Web Developer", "Coder"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
